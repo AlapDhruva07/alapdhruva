0261b425842d62bc499e813876d4e4fd9b15659c
Alap Dhruva <200420045@student.georgianc.on.ca>
2020-03-03 20:11:28 -0500

    react app version-1.1

---
+++ precache-manifest.25a9f9020a64fc338a6d9e22c905e257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a55a0386f2100651ce3ff7bae552f993",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dd0e54be975e65607be9",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e9c54524695e7e4430fb",
+    "url": "./static/js/2.4233ffa4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.4233ffa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd0e54be975e65607be9",
+    "url": "./static/js/main.d54a6b64.chunk.js"
+  },
+  {
+    "revision": "24c2a3a2774b1eb32f43",
+    "url": "./static/js/runtime-main.76913305.js"
+  }
+]);
